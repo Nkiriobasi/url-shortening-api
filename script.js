80baac803d874fd684490f3febf83dc4ba572aad
Nkiriobasi <obasinkiri@gmail.com>
2022-07-14 14:50:23 +0100

    implemented the url shortener functionality

--- script.js
+++ script.js
@@ -4,45 +4,63 @@ const selectElement = (selector) => {
     throw new Error(`Cannot find element ${selector}`);
 }
 
-const form = selectElement('form');
-const input = selectElement('input');
-const parentResult = selectElement('.link-result__container');
-
-const result = document.createElement('div');
-result.classList.add('result');
+const form = selectElement('#form');
+const input = selectElement('#urlinput');
+const linkRc = selectElement('#linkRc');
 
 
-form.addEventListener('submit', (e) => {
+
+form.addEventListener('submit', e => {
     e.preventDefault();
-    const url = input.value;
-    
-    const initialUrl = document.createElement('p');
-    initialUrl.classList.add('initialUrl');
-    initialUrl.innerHTML = url;
+    const inputUrlValue = input.value;
 
-    shortenUrl(url);
 
-    result.appendChild(initialUrl);
+    // send a POST request
+    const headers = {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    };
 
-    parentResult.appendChild(result);
-})
+    let body = {
+        "long_url": inputUrlValue,
+        "domain": "https://t.ly/",
+        "api_token": "57WpXH480Zc6hsr3Rf442NlQvHM3bEN7dS5wfu9Wu2Jr1Egoku3NmtPngbrW",
+        "include_qr_code": false
+    };
 
-async function shortenUrl(url){
-    try {
-        const res = await fetch(`https://api.shrtco.de/v2/shorten?url=${url}`);
-        const data = res.json();
 
-        console.log(data);
-
-        let newUrlWrap = document.createElement('div');
-        newUrlWrap.classList.add('newUrl-wrap');
-        newUrlWrap.innerHTML = `
-        <p class="newUrl">${data}</p>
-        <button class="newUrl-btn">Copy</button>
+    fetch("https://t.ly/api/v1/link/shorten", {
+        method: "POST",
+        headers,
+        body: JSON.stringify(body),
+    })
+    .then(response => {
+        if (!response.ok) { throw response }
+        return response.json()
+    })
+    .then(data => {
+        const longUrl = document.createElement('p');
+        longUrl.classList.add('longUrl');
+        longUrl.innerHTML = data.long_url;
+ 
+        const shortUrlWrap = document.createElement('div');
+        shortUrlWrap.classList.add('shortUrl-wrap');
+        shortUrlWrap.innerHTML = `
+        <p class="shortUrl">${data.short_url}</p>
+        <button class="shortUrl-btn">Copy</button>
         `
 
-        result.appendChild(newUrlWrap);
-    } catch (error) {
-        console.log(error);
-    }
-}
+        const result = document.createElement('div');
+        result.classList.add('result');
+        result.appendChild(longUrl);
+        result.appendChild(shortUrlWrap);
+
+        linkRc.appendChild(result);
+        
+        // clear input field
+        form.reset();
+    })
+    .catch(error => {
+        throw new Error(`Error: ${error}`);
+    });
+})
